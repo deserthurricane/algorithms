8c74b8f44797372ddf071272f3f20cb2b581651c
nastya <pr.polishchuk@gmail.com>
2021-07-15 22:40:59 +0300

    Алгоритм Дейкстры

--- Dijkstra.ts
+++ Dijkstra.ts
@@ -10,76 +10,89 @@ class Dijkstra<V> {
     this.matrix = graph.getMatrix();
   }
 
-  main(): VerticeWeight[] {
-    const start = new VerticeWeight(0, 0);
-    const minWay: VerticeWeight[] = [start];
-    const checkedVertices = [start];
-    const visited: number[] = [];
+  main(): number[] {
+    const start = new VerticeWeight(0, 0); // начальная вершина имеет нулевые индекс и стоимость пути к ней
+    const currentWeightVertices = [start]; // массив для хранения вершин с их текущими весами и истоками. важно, чтобы индексы совпадали с this.verticeArray
+    const visited: number[] = []; // здесь храним индексы посещенных вершин
 
-    let currentMinIndex = 0;
+    let currentMinWayIndex = 0; // вершина, которую сейчас проверяем
 
-    // как понять, что мы полностью проверили вершину?
-
-    while (currentMinIndex < this.verticeArray.length) {
-      // visited.push(currentMinIndex);
-      // console.log('hi');
-      
+    while (currentMinWayIndex < this.verticeArray.length) {
       for (let i = 0; i < this.verticeArray.length; i++) {
-        // Если вершины смежные
-        if (this.matrix[currentMinIndex][i] > 0) {
-          const curWayWeight = checkedVertices[currentMinIndex]?.getWeight() ?? 0;
+        if (this.matrix[currentMinWayIndex][i] > 0) {
+          const curWayWeight = currentWeightVertices[currentMinWayIndex]?.getWeight() ?? 0;
 
-          if (!checkedVertices[i]) {
-            // Как вернуться назад? Как сделать ссылку? Запустить рекурсивно?
-            if (currentMinIndex === 4 && i === 6) {
-              console.log(this.matrix[currentMinIndex][i], 'E - g');
-              console.log(curWayWeight, 'curWayWeight');
-              
-            } 
-            checkedVertices[i] = new VerticeWeight(currentMinIndex, curWayWeight + this.matrix[currentMinIndex][i]);
+          if (!currentWeightVertices[i]) {
+            // Добавляем вершину впервые
+            currentWeightVertices[i] = new VerticeWeight(currentMinWayIndex, curWayWeight + this.matrix[currentMinWayIndex][i]);
           } else {
-            // const curWayWeight = checkedVertices[currentMinIndex].getWeight();
-            const newWeight = curWayWeight + this.matrix[currentMinIndex][i];
-            if (currentMinIndex === 4 && i === 6) {
-              console.log(this.matrix[currentMinIndex][i], 'E - g');
-            }          
-            if (checkedVertices[i].getWeight() > newWeight) {
-              checkedVertices[i].setUnion(currentMinIndex, newWeight);
+            // Ищем другие пути к вершине и сравниваем с ее текущим весом
+            const newWeight = curWayWeight + this.matrix[currentMinWayIndex][i];
+         
+            if (currentWeightVertices[i].getWeight() > newWeight) {
+              // Если есть более "дешевый" вариант - перезаписываем текущий вес и исток
+              currentWeightVertices[i].setUnion(currentMinWayIndex, newWeight);
             }
           }
         }        
       }
 
-      // if (currentMinIndex === 4) {
-      //   console.log(checkedVertices, 'checkedVertices');
-      // }
-
-      // let minValue;
-      // let newCurrentIndex = currentMinIndex + 1; 
-    
-      // for (let i = 0; i < this.verticeArray.length; i++) {
-      //   if (this.matrix[currentMinIndex][i] !== 0) {
-      //     const weight = checkedVertices[i].getWeight();
-      //     if ((minValue > weight && !visited.includes(i) ) || !minValue) {
-      //       minValue = weight;
-      //       newCurrentIndex = checkedVertices[i].getVerticeIndex();
-      //     }
-      //   }
-      // }
-
-      // currentMinIndex = newCurrentIndex;
-          currentMinIndex++;
+      visited.push(currentMinWayIndex);
+      // Самый важный момент: выбираем следующую вершину - самую "дешевую" из смежных
+      currentMinWayIndex = this._getNextVerticeIndex(currentMinWayIndex, currentWeightVertices, visited);
     }
 
-    // console.log(currentMinIndex, 'currentMinIndex');
-    console.log(checkedVertices, 'checkedVertices');
+    console.log(visited, 'visited');
+    console.log(currentWeightVertices, 'currentWeightVertices');
 
+    const minWay = this._getMinWay(currentWeightVertices);
+    console.log(minWay, 'minWay');
+    
     return minWay;
+  }
+
+  /**
+   * Получение индекса следующей смежной вершины с минимальным весом
+   */
+  _getNextVerticeIndex(currentIndex: number, checkedVertices: VerticeWeight[], visited: number[]): number {
+    let minValue;
+    let newCurrentIndex = currentIndex + 1;
+
+    for (let i = 0; i < this.verticeArray.length; i++) {
+      if (this.matrix[currentIndex][i] !== 0 && !visited.includes(i)) {
+        const weight = checkedVertices[i].getWeight();
+        if (minValue > weight || !minValue) {
+          minValue = weight;
+          newCurrentIndex = i;
+        }
+      }
+    }
+
+    return newCurrentIndex;
+  }
+
+  /**
+   * Получение последовательности прохода по вершинам
+   */
+  _getMinWay(checkedVertices: VerticeWeight[]): number[] {
+    let i = this.verticeArray.length - 1; // начинаем собирать кратчайший путь с последней вершины ("место назначения")
+    const minWay = [i];
+
+    while (i > 0) {
+      const prevVerticeIndex = checkedVertices[i].getPrevVerticeIndex();
+      i = prevVerticeIndex;
+      minWay.push(i);
+    }
+    
+    return minWay.reverse();
   }
 }
 
+/**
+ * Структура для хранения индекса предыдущей вершины
+ * и стоимости пути к текущей вершине
+ */
 class VerticeWeight {
-  // индекс вершины, из которой мы пришли, и стоимость пути
   private union: [number, number];
 
   constructor(source: number, weight: number) {
@@ -94,7 +107,7 @@ class VerticeWeight {
     return this.union[1];
   }
 
-  public getVerticeIndex() {
+  public getPrevVerticeIndex() {
     return this.union[0];
   }
 }
