19a61cf9bef7c116bd53c28a8c76bea706ca6a03
Anastasia Polishchuk <apolishchuk@inno.tech>
2021-09-19 21:34:30 +0300

    Алгоритм корректно работает

--- LZ77-2.ts
+++ LZ77-2.ts
@@ -1,14 +1,14 @@
 // Алгоритм Lempel-Ziv 1977 - динамическое накопление словаря при сжатии данных
 
-type CharTableValue = [number, number, string];
+type CharTableValue = [number, number, string | null];
 
 class LZ77 {
-  text = '';
+  text: string;
   charTable: Array<CharTableValue> = [];
   bufferLength = 4;
-  dictLength = 11; // вопрос! как правильно выбрать размер?
+  dictLength = 6; // вопрос! как правильно выбрать размер?
 
-  constructor(text) {
+  constructor(text: string) {
     this.text = text;
   }
 
@@ -17,9 +17,10 @@ class LZ77 {
 
     while (cursor < this.text.length) {
       const currentChar = this.text[cursor];
+      const dictIdxArr = this.getAllCoincidenceIdxFromCharTable(cursor);
 
-      if (this.hasCharTableCurrentCharInWindow(currentChar, cursor)) {
-        const [position, length] = this.findLongestSubstring(cursor);
+      if (dictIdxArr?.length > 0) {
+        const [position, length] = this.findLongestSubstring(cursor, dictIdxArr);
         const newChar = this.text[cursor + length] || null; // null - если строка закончилась
         this.charTable.push([position, length, newChar]);
         cursor += length + 1;
@@ -32,34 +33,14 @@ class LZ77 {
     }
   }
 
-  // есть ли в словаре в интервале скользящего окна искомый символ
-  hasCharTableCurrentCharInWindow(char: string, cursor: number) {
-    const start = cursor - this.dictLength > 0 ? cursor - this.dictLength : 0;
-    return this.charTable.slice(start).find(value => value[2] === char);
+  /**
+   * Насколько окно словаря смещено относительно начала строки
+   */
+  getWindowOffset(cursor: number): number {
+    return cursor - this.dictLength > 0 ? cursor - this.dictLength : 0;
   }
 
-  findLongestSubstring(cursor: number) {
-    console.log(cursor, 'cursor');
-
-    const start = cursor - this.dictLength > 0 ? cursor - this.dictLength : 0;
-
-    console.log(start, 'start');
-
-
-    const dictIdxArr: number[] = this.text
-      .slice(start, cursor)
-      .split('')
-      .reduce((accum, current, currentIndex) => {
-        if (current === this.text[cursor]) {
-          accum.push(currentIndex);
-        }
-
-        return accum;
-      }, []);
-
-    console.log(dictIdxArr, 'dictIdxArr');
-
-
+  findLongestSubstring(cursor: number, dictIdxArr: number[]) {
     const results = new Map<number, number>();
 
     for (let i = 0; i < dictIdxArr.length; i++) {
@@ -77,24 +58,59 @@ class LZ77 {
       results.set(position, count);
     }
 
-    console.log(results, 'results');
-
-
+    // console.log(results, 'results');
     //  const longestSubStr = Object.entries(results).sort(([key1, value1], [key2, value2]) => value2 > value1 ? 1 : -1)[0]
-    console.log(Array.from(Object.entries(results)));
+    // console.log(Array.from(Object.entries(results)));
 
     const longestSubStr = Array.from(results.entries()).sort(([key1, value1], [key2, value2]) => value2 > value1 ? 1 : -1)[0];
-    console.log(longestSubStr, 'longestSubStr');
+    // console.log(longestSubStr, 'longestSubStr');
 
     return longestSubStr;
   }
+  /**
+   * Находим все совпадения в charTable
+   */
+  getAllCoincidenceIdxFromCharTable(cursor: number): number[] {
+    // console.log(cursor, 'cursor');
+    const windowOffset = this.getWindowOffset(cursor);
+    // console.log(windowOffset, 'windowOffset');
 
+    const dictIdxArr: number[] = this.text
+      .slice(windowOffset, cursor)
+      .split('')
+      .reduce((accum, current, currentIndex) => {
+        if (current === this.text[cursor]) {
+          accum.push(currentIndex + windowOffset); // получаем "абсолютный" индекс из исходной строки
+        }
+
+        return accum;
+      }, []);
+
+    // console.log(dictIdxArr, 'dictIdxArr');
+    return dictIdxArr;
+  }
+
+  /**
+   * Встречается ли в словаре в интервале скользящего окна искомый символ
+   * @TODO оптимизировать ???, так как позже мы проходим по той же строке в getAllCoincidenceIdxFromCharTable 
+   */
+  // hasCharTableCurrentCharInWindow(char: string, cursor: number): boolean {
+  //   // Проход до первого совпадения, чтобы получить булевый результат и сэкономить количество итераций
+  //   return this.charTable.some(value =>
+  //     value[2] === char // проверка на наличие символа в словаре - в общем
+  //     && value[1] <= this.dictLength // попадает ли найденный символ в текущий размер окна - в частности
+  //   );
+  // }
 }
 
-
-
+/**
+ * ТЕСТЫ
+ */
 const algor = new LZ77('ABRAKADABRA');
 algor.encode();
 
 const algor2 = new LZ77('aacaacabcabaaac');
 algor2.encode();
+
+const algor3 = new LZ77('КОСИЛКОСОЙКОСОЙКОСОЙ');
+algor3.encode();
