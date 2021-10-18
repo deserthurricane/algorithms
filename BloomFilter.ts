cd4656a7c2a154747a2074c661453c7fbfe31f16
Anastasia Polishchuk <apolishchuk@inno.tech>
2021-10-18 12:29:56 +0300

    Добавлено определение размера массива и количества хэш-функций

--- BloomFilter.ts
+++ BloomFilter.ts
@@ -1,21 +1,41 @@
 const chalk = require('chalk');
+const murmurHash3 = require('murmurhash3js');
 
 /**
  * Фильтр Блума
  * Вероятностная структура данных, позволяющая проверять принадлежность элемента к множеству
  */
 class BloomFilter {
+  /** Количество хранимых в фильтре значений */
+  private elementsCount: number;
+  /** Количество допустимых false positives */
+  private probability: number;
   /** Количество хэш-функций */
   private hashFnCount: number;
   /** Буфер для хранения значений Блум-фильтра */
   private buffer: ArrayBuffer;
-  /** Максимальное значение для хранения данных в Блум-фильтре, в байтах */
+  /** Максимальное значение для хранения данных в Блум-фильтре, в битах */
   private size: number;
 
-  constructor(hashFnCount: number, size: number) {
-    this.hashFnCount = hashFnCount;
-    this.buffer = new ArrayBuffer(size);
-    this.size = size;
+  constructor(elementsCount: number, probability: number) {
+    this.elementsCount = elementsCount;
+    this.probability = probability;
+
+    this.getSize();
+    this.getHashFnCount();
+    this.buffer = new ArrayBuffer(Math.round(this.size / 8));
+    
+    console.log(this.buffer.byteLength, 'this.buffer.byteLength');
+    console.log(this.size, 'size in bits');
+    console.log(this.hashFnCount, 'hashFnCount');
+  }
+
+  private getSize() {
+    this.size = Math.round(-(this.elementsCount * Math.log(this.probability))/(Math.pow(Math.log(2), 2)));
+  }
+
+  private getHashFnCount() {
+    this.hashFnCount = Math.round((this.size/this.elementsCount) * Math.log(2));
   }
 
   /**
@@ -38,6 +58,7 @@ class BloomFilter {
    * Получение одного хэш-кода
    */
   private getHashCode(value: string, i: number): number {
+    // return murmurHash3.x86.hash32(value, i);
     return value
       .split('')
       .map(char => char.charCodeAt(0) + i)
@@ -73,7 +94,7 @@ class BloomFilter {
 }
 
 /** TEST */
-const bloomFilter = new BloomFilter(6, 256);
+const bloomFilter = new BloomFilter(20, 0.05);
 
 const wordPresent = ['abound', 'abounds', 'abundance', 'abundant', 'accessable',
   'bloom', 'blossom', 'bolster', 'bonny', 'bonus', 'bonuses',
@@ -87,11 +108,12 @@ const wordAbsent = ['bluff', 'cheater', 'hate', 'war', 'humanity',
   'racism', 'hurt', 'nuke', 'gloomy', 'facebook',
   'geeksforgeeks', 'twitter'];
 
-const testWords = ['abound', 'bluff', 'abundance', 'abundant', 'accessable',
+const testWords = ['abound', 'bluff', 'cheater', 'hate', 'war', 'humanity',
+'racism', 'abundance', 'abundant', 'accessable',
   'bloom', 'blossom', 'bolster', 'bonny', 'hurt', 'nuke', 'gloomy', 'facebook',
   'geeksforgeeks', 'twitter'].sort(() => (Math.random() > 0.5) ? 1 : -1);
 
-// 1 результат из 15 (bluff) оказывается ложно положительным
+// 1 результат из 20 (cheater) оказывается ложно положительным
 testWords.forEach(word => {
   const isFound = bloomFilter.checkValue(word);
   const isPresent = wordPresent.includes(word);
